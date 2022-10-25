03033f475fb203910945b5089623936545821b78
chaz Graham <chazhg80.cg@gmail.com>
2022-10-24 21:05:56 -0500

    changed force to false

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.use('./uploads', express.static('uploads'));
 
 app.use(require('./controllers/'));
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
